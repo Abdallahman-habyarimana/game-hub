a7e630483081dced2ba9babc876d04bd688d5f0d
Abdallahman Habyarimana <habdallah005@gmail.com>
2024-02-16 22:19:42 -0500

    design responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 
 
 import React from 'react'
+import { Grid, GridItem, Show} from '@chakra-ui/react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Grid templateAreas={{
+      base: `"nav" main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area="nav" bg="coral">
+          Nav
+      </GridItem>
+      <Show above="lg">
+      <GridItem area="aside" bg="coral">
+          Aside
+      </GridItem>
+      </Show>
+      <GridItem area="main" bg="coral">
+          Main
+      </GridItem>
+    </Grid>
   )
 }
 
